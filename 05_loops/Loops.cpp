e5daf0006bed0c77e2f1833e6cdf7fca8c37d835
AdnanRohmatKurniansah <atnandimas@gmail.com>
2025-06-06 21:45:10 +0700

    while

---
+++ 05_loops/Loops.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    // While
+    // int countdown = 3;
+
+    // while (countdown > 0) {
+    //     cout << countdown << endl;
+    //     countdown--;
+    // }
+
+    // cout << "Happy New Year" << endl;
+
+    
+    int i = 0;
+    do {
+        cout << i << endl;
+        i++;
+    }
+    while (i < 5);
+}   
