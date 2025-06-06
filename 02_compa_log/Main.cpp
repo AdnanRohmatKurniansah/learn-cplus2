2a924b50aee3fb65dc038d27c7f174ece8a7c02b
AdnanRohmatKurniansah <atnandimas@gmail.com>
2025-06-06 14:43:52 +0700

    comparison and logical

---
+++ 02_compa_log/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 3;
+    cout << (x > y) << endl;
+
+    int a = 5;
+    int b = 3;
+    cout << (a > 3 || b < 4); 
+    return 0;
+}
