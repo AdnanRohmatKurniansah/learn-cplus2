669257805e23fb2b2b0727c95f74dca6c919b2e9
AdnanRohmatKurniansah <atnandimas@gmail.com>
2025-06-06 20:41:56 +0700

    math

---
+++ 03_math/Math.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+    // max
+    cout << "Max: " << max(5, 10) << endl;
+    // min 
+    cout << "Min: " << min(1, 3) << endl;
+    // square root 
+    cout << "Akar dari 64: " << sqrt(64) << endl;
+    // round 
+    cout << "Pembulatan dari 2.3: " << round(2.3) << endl;
+    // logarithm
+    cout << "Hasil dari log 8: " << log(8) << endl;
+
+    return 0;
+}
