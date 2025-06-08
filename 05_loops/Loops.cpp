19240169ac87df7103f3f5377fb9ea0898660ba7
AdnanRohmatKurniansah <atnandimas@gmail.com>
2025-06-08 14:35:44 +0700

    for foreach

--- 05_loops/Loops.cpp
+++ 05_loops/Loops.cpp
@@ -4,20 +4,37 @@ using namespace std;
 
 int main () {
     // While
-    // int countdown = 3;
+    int countdown = 3;
+    while (countdown > 0) {
+        cout << countdown << endl;
+        countdown--;
+    }
 
-    // while (countdown > 0) {
-    //     cout << countdown << endl;
-    //     countdown--;
-    // }
+    cout << "Happy New Year" << endl;
 
-    // cout << "Happy New Year" << endl;
-
-    
+    // Do while
     int i = 0;
     do {
         cout << i << endl;
         i++;
     }
     while (i < 5);
+
+    // // for
+    int sum = 0;    
+    for (int i = 1; i <= 5; i++)
+    {
+        sum = sum + 1;
+        cout << "Sum is " << sum << endl;
+    }
+
+
+    // foreach
+    int numbers[3] = {1, 2, 3};
+    for (int i: numbers)
+    {
+        cout << i << endl;
+    }
+    
+    
 }   
