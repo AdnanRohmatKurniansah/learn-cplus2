c8600a91607975abcc1e64d8632f57b738f80390
AdnanRohmatKurniansah <atnandimas@gmail.com>
2025-06-11 21:14:51 +0700

    lat faktorial

---
+++ lat_faktorial/Faktorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int faktorial(int number) {
+    if (number <= 1) {
+        cout << number;
+        return number;
+    } else {
+        cout << number;
+        return number * faktorial(number - 1);
+    }
+}
+
+int main() {
+    int number, result;
+
+    cout << "Menghitung faktorial: ";
+    cin >> number;
+
+    result = faktorial(number);
+    cout << "\nNilai faktorialnya adalah: " << result << endl;
+
+    return 0;
+}
+
